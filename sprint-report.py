cc67b12d1d79ef9488e7d9bcd1246e2a32aad272
Brinda-T <brindathanikanti1998@gmail.com>
2021-06-21 10:24:29 +0530

    changed code from excel-excel to csv-excel by taking inputs as csv files and processed into excel

--- sprint-report.py
+++ sprint-report.py
@@ -1,3 +1,5 @@
+import csv
+from openpyxl import Workbook
 from openpyxl import load_workbook
 from openpyxl.styles import Font, Fill, colors, PatternFill, Protection, Alignment
 from openpyxl.styles import NamedStyle
@@ -104,23 +106,30 @@ remarks_gen_column_name = columns.get_remarks_gen_column_name()
 sr_debug ("remarks_gen_column_name", remarks_gen_column_name)
 
 
-def print_all_worksheet_names(wbname):
+def create_workbook(wbook):
+   
+    try:
+        wb = load_workbook(wbook)
+        print("Workbook '%s'exists" %(wb))
+    except:
+        print("Creating worksheet: '%s'" %(wbook))
+        wb = Workbook()
+
+    wb.save(wbook)
+
+def get_read_csv_files(file_name):
+    datalist = []
+    fd = open(file_name)
+
+    fname = csv.reader(fd)
+    for row in fname:
+        datalist.append(row)
+    return datalist
+
+def create_worksheet(wbname, dst_wname):
+
     wb = load_workbook(wbname)
 
-    sr_debug (wb.sheetnames)
-
-    for wsheet in wb.sheetnames:
-        sr_debug (wsheet)
-    sr_debug (" ")
-
-def print_row_column1(wbname,src_wname1,src_wname2,dst_wname):
-    dates_list = []
-    sprint_list = []
-
-    wb = load_workbook(wbname)
-
-    swsheet1 = wb.get_sheet_by_name(src_wname1)
-    swsheet2 = wb.get_sheet_by_name(src_wname2)
 
     try:
         dwsheet = wb.get_sheet_by_name(dst_wname)
@@ -131,24 +140,32 @@ def print_row_column1(wbname,src_wname1,src_wname2,dst_wname):
         sr_debug ("worksheet '%s' not found"%(dst_wname))
     finally:
         sr_debug ("creating new worksheet:'%s'"%(dst_wname))
-        dwsheet = wb.create_sheet(dst_wname,0) 
+        dwsheet = wb.create_sheet(dst_wname,0)
+	
+    wb.save(wbname)
+def print_row_column1(wbname,stories,epics,dst_wname):
+    dates_list = []
+    sprint_list = []		
+    wb = load_workbook(wbname)
+    dwsheet = wb.get_sheet_by_name(dst_wname)
 
-    srcount1 = swsheet1.max_row
-    sccount1 = swsheet1.max_column
-    sr_debug ("%s: max row:col (%d:%d)"%(src_wname1,srcount1,sccount1))
+    srcount1 = len(stories)
+    
+    sr_debug ("stories length :%d)"%(srcount1))
 
-    srcount2 = swsheet2.max_row
-    sccount2 = swsheet2.max_column
-    sr_debug ("%s: max row:col (%d:%d)"%(src_wname2,srcount2,sccount2))
-
+    srcount2 = len(epics)
+    
+    sr_debug ("epics length :%d)"%(srcount2))
+    
+    dwsheet.insert_cols(1, 18)
     #drcount =  dwsheet.max_row
     #dccount =  dwsheet.max_column
     #sr_debug("%s: max row:col (%d:%d)" % (dst_wname, drcount, dccount))
-
-    row = swsheet1[1]       
-    frow = [cell.value for cell in row]
-    dwsheet.append(frow)
-
+    '''
+    row = stories[0]       
+    for frow in row:
+        dwsheet.append(frow)
+    '''
     titles_center_aligned_text = Alignment(horizontal = "center", vertical = "center", wrapText=True)
     center_aligned_text = Alignment(horizontal = "center", vertical = "center")
 
@@ -214,87 +231,92 @@ def print_row_column1(wbname,src_wname1,src_wname2,dst_wname):
     dwsheet['s1'] = remarks_gen_column_name
     dwsheet['s1'].alignment = titles_center_aligned_text
 
-    for j in range (2, srcount1+1):
-        sprint_list.append(swsheet1[j][sprint_pos].value)
-        sprint_list.append(swsheet1[j][sprint2_pos].value)
-        sprint_list.append(swsheet1[j][sprint3_pos].value)
+    for j in range (1, srcount1):
+        
+        i = j + 1
+
+        sprint_list.append(stories[j][sprint_pos])
+        sprint_list.append(stories[j][sprint2_pos])
+        sprint_list.append(stories[j][sprint3_pos])
         sprint_list = list(filter(None, sprint_list))
         sprint_list.sort(reverse = True)
         #sr_debug (sprint_list)
-        dwsheet[j][issue_id_pos].value = sprint_list[0]
+        dwsheet[i][issue_id_pos].value = sprint_list[0]
         sprint_list = []
-    
-    for i in range (2, srcount1+1):
-        dwsheet[i][sno_pos].value = swsheet1[i][sno_pos].value
-        dwsheet[i][sno_pos].alignment = center_aligned_text
 
-        dwsheet[i][issue_key_pos].value = swsheet1[i][custom_field_epiclink_pos].value
-        dwsheet[i][issue_key_pos].alignment = center_aligned_text
+    for i in range (1, srcount1):
+        j = i + 1
+        dwsheet[j][sno_pos].value = stories[i][sno_pos]
+        dwsheet[j][sno_pos].alignment = center_aligned_text
 
-        dwsheet[i][custom_field_epiclink_pos].value = swsheet1[i][issue_key_pos].value
-        dwsheet[i][custom_field_epiclink_pos].alignment = center_aligned_text
+        dwsheet[j][issue_key_pos].value = stories[i][custom_field_epiclink_pos]
+        dwsheet[j][issue_key_pos].alignment = center_aligned_text
 
-        dwsheet[i][ename_pos].value = swsheet1[i][summary_pos].value
-        dwsheet[i][ename_pos].alignment = center_aligned_text
+        dwsheet[j][custom_field_epiclink_pos].value = stories[i][issue_key_pos]
+        dwsheet[j][custom_field_epiclink_pos].alignment = center_aligned_text
 
-        dwsheet[i][assignee_pos].value = swsheet1[i][assignee_pos].value
-        dwsheet[i][assignee_pos].alignment = center_aligned_text
+        dwsheet[j][ename_pos].value = stories[i][summary_pos]
+        dwsheet[j][ename_pos].alignment = center_aligned_text
 
-        dwsheet[i][custom_field_storypoints_pos].value = swsheet1[i][custom_field_storypoints_pos].value
-        dwsheet[i][custom_field_storypoints_pos].alignment = center_aligned_text
+        dwsheet[j][assignee_pos].value = stories[i][assignee_pos]
+        dwsheet[j][assignee_pos].alignment = center_aligned_text
 
-        estimate_effort_hours = int(swsheet1[i][original_estimate_pos].value) / 3600
-        dwsheet[i][time_spent_pos].value = estimate_effort_hours
-        dwsheet[i][time_spent_pos].alignment = center_aligned_text
+        dwsheet[j][custom_field_storypoints_pos].value = stories[i][custom_field_storypoints_pos]
+        dwsheet[j][custom_field_storypoints_pos].alignment = center_aligned_text
 
-        dwsheet[i][remaining_estimate_pos].value = swsheet1[i][time_spent_pos].value
-        dwsheet[i][remaining_estimate_pos].alignment = center_aligned_text
+        estimate_effort_hours = int(stories[i][original_estimate_pos]) / 3600
+        dwsheet[j][time_spent_pos].value = estimate_effort_hours
+        dwsheet[j][time_spent_pos].alignment = center_aligned_text
 
-        effort_consumed_hours = int(swsheet1[i][remaining_estimate_pos].value) / 3600
-        dwsheet[i][sprint_pos].value = effort_consumed_hours
-        dwsheet[i][sprint_pos].alignment = center_aligned_text
+        dwsheet[j][remaining_estimate_pos].value = stories[i][time_spent_pos]
+        dwsheet[j][remaining_estimate_pos].alignment = center_aligned_text
+
+        effort_consumed_hours = int(stories[i][time_spent_pos]) / 3600
+        dwsheet[j][sprint_pos].value = effort_consumed_hours
+        dwsheet[j][sprint_pos].alignment = center_aligned_text
 
         pending_effort_hours = estimate_effort_hours - effort_consumed_hours
-        dwsheet[i][sprint2_pos].value = pending_effort_hours
-        dwsheet[i][sprint2_pos].alignment = center_aligned_text
+        dwsheet[j][sprint2_pos].value = pending_effort_hours
+        dwsheet[j][sprint2_pos].alignment = center_aligned_text
 
         effort_completion = (effort_consumed_hours/estimate_effort_hours) * 100
-        dwsheet[i][progress_pos].value = ('{}{}'.format(round(effort_completion),"%"))
-        dwsheet[i][progress_pos].alignment = center_aligned_text
+        dwsheet[j][progress_pos].value = ('{}{}'.format(round(effort_completion),"%"))
+        dwsheet[j][progress_pos].alignment = center_aligned_text
 
         scheduled_progress = 100
-        dwsheet[i][scheduled_progress_pos].value = ('{}{}'.format(scheduled_progress,"%"))
-        dwsheet[i][scheduled_progress_pos].alignment = center_aligned_text
+        dwsheet[j][scheduled_progress_pos].value = ('{}{}'.format(scheduled_progress,"%"))
+        dwsheet[j][scheduled_progress_pos].alignment = center_aligned_text
 
         scheduled_overrun = 0
-        dwsheet[i][scheduled_overrun_pos].value = ('{}{}'.format(scheduled_overrun,"%"))
-        dwsheet[i][scheduled_overrun_pos].alignment = center_aligned_text
+        dwsheet[j][scheduled_overrun_pos].value = ('{}{}'.format(scheduled_overrun,"%"))
+        dwsheet[j][scheduled_overrun_pos].alignment = center_aligned_text
 
 
-    for k in range (2, srcount2+1):
-        for l in range (2, srcount1+1):
-            dates_list.append(swsheet2[k][epics_Esdate_pos].value)
-            dates_list.append(swsheet2[k][epics_Etdate_pos].value)
-            dates_list.append(swsheet2[k][epics_EASdate_pos].value)
-            dates_list.append(swsheet2[k][epics_EAEdate_pos].value)
+    for k in range (1, srcount2):
+        for l in range (1, srcount1):
+            i = l + 1
+            dates_list.append(epics[k][epics_Esdate_pos])
+            dates_list.append(epics[k][epics_Etdate_pos])
+            dates_list.append(epics[k][epics_EASdate_pos])
+            dates_list.append(epics[k][epics_EAEdate_pos])
             dates_list.sort()
 
-            if (swsheet2[k][epics_custom_field_pos].value):
-                swsheet1[l][custom_field_epiclink_pos].value
+            if (epics[k][epics_custom_field_pos]):
+                stories[l][custom_field_epiclink_pos]
 
-            dwsheet[l][teste_pos].value = datetime.datetime.date(dates_list[0])
-            dwsheet[l][teste_pos].alignment = center_aligned_text
+            dwsheet[i][teste_pos].value = (dates_list[0])
+            dwsheet[i][teste_pos].alignment = center_aligned_text
             #sr_debug (dates_list[0])
             #sr_debug (type(dates_list[0]))
 
-            dwsheet[l][original_estimate_pos].value = datetime.datetime.date(dates_list[2])
-            dwsheet[l][original_estimate_pos].alignment = center_aligned_text
+            dwsheet[i][original_estimate_pos].value = (dates_list[2])
+            dwsheet[i][original_estimate_pos].alignment = center_aligned_text
 
-            dwsheet[l][sprint3_pos].value = datetime.datetime.date(dates_list[1])
-            dwsheet[l][sprint3_pos].alignment = center_aligned_text
+            dwsheet[i][sprint3_pos].value = (dates_list[1])
+            dwsheet[i][sprint3_pos].alignment = center_aligned_text
 
-            dwsheet[l][summary_pos].value = datetime.datetime.date(dates_list[3])
-            dwsheet[l][summary_pos].alignment = center_aligned_text
+            dwsheet[i][summary_pos].value = (dates_list[3])
+            dwsheet[i][summary_pos].alignment = center_aligned_text
             #sr_debug (dwsheet[l][original_estimate_pos].value)
 
 
@@ -304,17 +326,21 @@ def print_row_column1(wbname,src_wname1,src_wname2,dst_wname):
     sr_debug("%s: max row:col (%d:%d)" % (dst_wname, drcount, dccount))
 
     sr_debug("Saving :%s" % (wbname))
-    wb.save("wrap.xlsx")
+    wb.save("employee-details.xlsx")
     wb.save(wbname)
 
 
 def main():
     filename = "employee-details.xlsx"
-    src_wname1 = "02-stories"
-    src_wname2 = "01-epics"
+    stories = "02-stories.csv"
+    epics = "01-epics.csv"
     dst_wname = "generated"
-    print_all_worksheet_names(filename)
-    print_row_column1(filename,src_wname1,src_wname2,dst_wname)
+    create_workbook(filename)
+    epics_data = get_read_csv_files(epics)
+    stories_data = get_read_csv_files(stories)
+    create_worksheet(filename, dst_wname)
+    #print_all_worksheet_names(filename)
+    print_row_column1(filename, stories_data, epics_data, dst_wname)
 
 if (__name__ == '__main__'):
     main()
