c4cf9c99e3f32d2395e66978e1b06996a346dd08
settypallidheeraj98 <settypallidheeraj@gmail.com>
2021-06-23 09:04:59 +0530

    text-alignment function is added to the project

--- sprint-report.py
+++ sprint-report.py
@@ -9,16 +9,16 @@ from sr_log import sr_log_messages, sr_debug
 
 sno_pos = columns.get_sno_pos()
 sr_debug("sno_pos", sno_pos)
+#sno_col_name = columns.get_sno_column_name()
+#sr_debug("sno_col_name", sno_col_name)
 
 issue_key_pos = columns.get_issue_key_pos()
 sr_debug ("issue_key_pos", issue_key_pos)
-
 issue_key_col_name = columns.get_issue_key_col_name()
 sr_debug ("issue_key_col_name", issue_key_col_name)
 
 issue_id_pos = columns.get_issue_id_pos()
 sr_debug ("issue_id_pos", issue_id_pos)
-
 issue_id_col_name = columns.get_issue_id_col_name()
 sr_debug ("issue_id_col_name", issue_id_col_name)
 custom_field_epiclink_pos = columns.get_custom_field_epiclink_pos()
@@ -127,10 +127,7 @@ def get_read_csv_files(file_name):
     return datalist
 
 def create_worksheet(wbname, dst_wname):
-
     wb = load_workbook(wbname)
-
-
     try:
         dwsheet = wb.get_sheet_by_name(dst_wname)
         sr_debug ("worksheet '%s' found"%(dst_wname))
@@ -143,6 +140,7 @@ def create_worksheet(wbname, dst_wname):
         dwsheet = wb.create_sheet(dst_wname,0)
 	
     wb.save(wbname)
+
 def get_col_names_and_values(wbname,stories,epics,dst_wname):
     dates_list = []
     sprint_list = []		
@@ -150,91 +148,39 @@ def get_col_names_and_values(wbname,stories,epics,dst_wname):
     dwsheet = wb.get_sheet_by_name(dst_wname)
 
     srcount1 = len(stories)
-    
     sr_debug ("stories length :%d)"%(srcount1))
 
     srcount2 = len(epics)
-    
     sr_debug ("epics length :%d)"%(srcount2))
     
     dwsheet.insert_cols(1, 18)
     #drcount =  dwsheet.max_row
     #dccount =  dwsheet.max_column
     #sr_debug("%s: max row:col (%d:%d)" % (dst_wname, drcount, dccount))
-    '''
-    row = stories[0]       
-    for frow in row:
-        dwsheet.append(frow)
-    '''
-    titles_center_aligned_text = Alignment(horizontal = "center", vertical = "center", wrapText=True)
-    center_aligned_text = Alignment(horizontal = "center", vertical = "center")
-
-    '''     
-    dwsheet['A1'] = "sai\ndheeraj"
-    dwsheet['A1'].alignment = Alignment(horizontal = "center", vertical = "center", wrapText=True)
-    #dwsheet['A1'].alignment = center_aligned_text
-    wb.save("wrap.xlsx")
-    '''
-
+       
     header_row = dwsheet[1]
+    #dwsheet[1][sno_pos].value = sno_col_name
     dwsheet[1][issue_key_pos].value = issue_key_col_name
-    dwsheet[1][issue_key_pos].alignment = titles_center_aligned_text
-
     dwsheet[1][issue_id_pos].value = issue_id_col_name
-    dwsheet[1][issue_id_pos].alignment = titles_center_aligned_text
-
     dwsheet[1][custom_field_epiclink_pos].value = custom_field_epiclink_col_name
-    dwsheet[1][custom_field_epiclink_pos].alignment = titles_center_aligned_text
-
     dwsheet[1][ename_pos].value = ename_col_name
-    dwsheet[1][ename_pos].alignment = titles_center_aligned_text
-
     dwsheet[1][assignee_pos].value = assignee_col_name
-    dwsheet[1][assignee_pos].alignment = titles_center_aligned_text
-
     dwsheet[1][custom_field_storypoints_pos].value = custom_field_storypoints_col_name
-    dwsheet[1][custom_field_storypoints_pos].alignment = titles_center_aligned_text
-
     dwsheet[1][teste_pos].value = teste_col_name
-    dwsheet[1][teste_pos].alignment = titles_center_aligned_text
-
     dwsheet[1][original_estimate_pos].value = original_estimate_col_name
-    dwsheet[1][original_estimate_pos].alignment = titles_center_aligned_text
-
     dwsheet[1][time_spent_pos].value = time_spent_col_name
-    dwsheet[1][time_spent_pos].alignment = titles_center_aligned_text
-
     dwsheet[1][remaining_estimate_pos].value = remaining_estimate_col_name
-    dwsheet[1][remaining_estimate_pos].alignment = titles_center_aligned_text
-
     dwsheet[1][sprint_pos].value = sprint_col_name
-    dwsheet[1][sprint_pos].alignment = titles_center_aligned_text
-
     dwsheet[1][sprint2_pos].value = sprint2_col_name
-    dwsheet[1][sprint2_pos].alignment = titles_center_aligned_text
-
     dwsheet[1][sprint3_pos].value = sprint3_col_name
-    dwsheet[1][sprint3_pos].alignment = titles_center_aligned_text
-
     dwsheet[1][summary_pos].value = summary_col_name
-    dwsheet[1][summary_pos].alignment = titles_center_aligned_text
-
     dwsheet['p1'] = progress_gen_column_name
-    dwsheet['p1'].alignment = titles_center_aligned_text
-
     dwsheet['q1'] = scheduled_progress_gen_column_name
-    dwsheet['q1'].alignment = titles_center_aligned_text
-
     dwsheet['r1'] = scheduled_overrun_gen_column_name
-    dwsheet['r1'].alignment = titles_center_aligned_text
-
     dwsheet['s1'] = remarks_gen_column_name
-    dwsheet['s1'].alignment = titles_center_aligned_text
 
     for j in range (1, srcount1):
-        
         i = j + 1
-
         sprint_list.append(stories[j][sprint_pos])
         sprint_list.append(stories[j][sprint2_pos])
         sprint_list.append(stories[j][sprint3_pos])
@@ -247,51 +193,33 @@ def get_col_names_and_values(wbname,stories,epics,dst_wname):
     for i in range (1, srcount1):
         j = i + 1
         dwsheet[j][sno_pos].value = stories[i][sno_pos]
-        dwsheet[j][sno_pos].alignment = center_aligned_text
-
         dwsheet[j][issue_key_pos].value = stories[i][custom_field_epiclink_pos]
-        dwsheet[j][issue_key_pos].alignment = center_aligned_text
-
         dwsheet[j][custom_field_epiclink_pos].value = stories[i][issue_key_pos]
-        dwsheet[j][custom_field_epiclink_pos].alignment = center_aligned_text
-
         dwsheet[j][ename_pos].value = stories[i][summary_pos]
-        dwsheet[j][ename_pos].alignment = center_aligned_text
-
         dwsheet[j][assignee_pos].value = stories[i][assignee_pos]
-        dwsheet[j][assignee_pos].alignment = center_aligned_text
-
         dwsheet[j][custom_field_storypoints_pos].value = stories[i][custom_field_storypoints_pos]
-        dwsheet[j][custom_field_storypoints_pos].alignment = center_aligned_text
-
+        
         estimate_effort_hours = int(stories[i][original_estimate_pos]) / 3600
         dwsheet[j][time_spent_pos].value = estimate_effort_hours
-        dwsheet[j][time_spent_pos].alignment = center_aligned_text
 
         dwsheet[j][remaining_estimate_pos].value = stories[i][time_spent_pos]
-        dwsheet[j][remaining_estimate_pos].alignment = center_aligned_text
 
         effort_consumed_hours = int(stories[i][time_spent_pos]) / 3600
         dwsheet[j][sprint_pos].value = effort_consumed_hours
-        dwsheet[j][sprint_pos].alignment = center_aligned_text
 
         pending_effort_hours = estimate_effort_hours - effort_consumed_hours
         dwsheet[j][sprint2_pos].value = pending_effort_hours
-        dwsheet[j][sprint2_pos].alignment = center_aligned_text
         dwsheet['M3'] = "=J3-L3"
 
         effort_completion = (effort_consumed_hours/estimate_effort_hours) * 100
         dwsheet[j][progress_pos].value = ('{}{}'.format(round(effort_completion),"%"))
-        dwsheet[j][progress_pos].alignment = center_aligned_text
         dwsheet['P2'].value = "=L3/J3*100" 
 
         scheduled_progress = 100
         dwsheet[j][scheduled_progress_pos].value = ('{}{}'.format(scheduled_progress,"%"))
-        dwsheet[j][scheduled_progress_pos].alignment = center_aligned_text
 
         scheduled_overrun = 0
         dwsheet[j][scheduled_overrun_pos].value = ('{}{}'.format(scheduled_overrun,"%"))
-        dwsheet[j][scheduled_overrun_pos].alignment = center_aligned_text
 
 
     for k in range (1, srcount2):
@@ -307,18 +235,14 @@ def get_col_names_and_values(wbname,stories,epics,dst_wname):
                 stories[l][custom_field_epiclink_pos]
 
             dwsheet[i][teste_pos].value = (dates_list[0])
-            dwsheet[i][teste_pos].alignment = center_aligned_text
             #sr_debug (dates_list[0])
             #sr_debug (type(dates_list[0]))
 
             dwsheet[i][original_estimate_pos].value = (dates_list[2])
-            dwsheet[i][original_estimate_pos].alignment = center_aligned_text
 
             dwsheet[i][sprint3_pos].value = (dates_list[1])
-            dwsheet[i][sprint3_pos].alignment = center_aligned_text
 
             dwsheet[i][summary_pos].value = (dates_list[3])
-            dwsheet[i][summary_pos].alignment = center_aligned_text
             #sr_debug (dwsheet[l][original_estimate_pos].value)
 
 
@@ -344,7 +268,23 @@ def get_cell_colors_using_patternfill(wbname, wsheet):
     for i in range(0, ccount):
         dwsheet[1][i].fill = fill_pattern
     
-    
+    wb.save(wbname)
+
+def text_alignment(wbname, dst_wname):
+    wb = load_workbook(wbname)
+    dwsheet = wb.get_sheet_by_name(dst_wname)
+
+    row_count = dwsheet.max_row
+    column_count = dwsheet.max_column
+
+    titles_center_aligned_text = Alignment(horizontal = "center", vertical = "center", wrapText=True)
+    center_aligned_text = Alignment(horizontal = "center", vertical = "center")
+
+    for i in range (2, row_count+1):
+        for j in range (0, column_count):
+            dwsheet[1][j].alignment = titles_center_aligned_text
+            dwsheet[i][j].alignment = center_aligned_text
+
     wb.save(wbname)
 
 def get_heading(wbname,wsheet):
@@ -389,6 +329,7 @@ def main():
     #print_all_worksheet_names(filename)
     get_col_names_and_values(filename, stories_data, epics_data, dst_wname)
     get_cell_colors_using_patternfill(filename, dst_wname)
+    text_alignment(filename, dst_wname)
     get_heading(filename, dst_wname)
     get_cell_formulae(filename, dst_wname)
 if (__name__ == '__main__'):
